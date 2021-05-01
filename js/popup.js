827b4d33cc75b07567084b2ca42ff959137c5e5b
Martin Klestil <34619286+martinklestil@users.noreply.github.com>
2021-05-01 14:18:54 +0200

    popup
    img

--- js/popup.js
+++ js/popup.js
@@ -15,27 +15,32 @@ class Popup{
 
     start(){
         console.log("start " + this.theId)
-        let img = document.getElementById("popup_img");
+        let img = document.getElementById("popup_img-1");
+        let img2 = document.getElementById("popup_img-2");
         let name = document.getElementById("popup_name");
         let text = document.getElementById("popup_description")
 
         if(this.theId === "1"){
             img.src = this.data[0].img;
+            img2.src = this.data[0].img;
             name.innerHTML = this.data[0].name;
             text.innerHTML = this.data[0].description; 
         }
         else if(this.theId === "2"){
             img.src = this.data[1].img;
+            img2.src = this.data[1].img;
             name.innerHTML = this.data[1].name;
             text.innerHTML = this.data[1].description; 
         }
         else if(this.theId === "3"){
             img.src = this.data[2].img;
+            img2.src = this.data[2].img;
             name.innerHTML = this.data[2].name;
             text.innerHTML = this.data[2].description; 
         }
         else {
             img.src = this.data[0].img;
+            img2.src = this.data[0].img;
             name.innerHTML = this.data[0].name;
             text.innerHTML = this.data[0].description; 
         }
