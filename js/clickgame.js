b4b94104208a9625e4b46a1868f8c6c606c14d68
Martin Klestil <34619286+martinklestil@users.noreply.github.com>
2021-05-01 23:00:58 +0200

    game 1
    Game 1 made playable.

---
+++ js/clickgame.js
@@ -0,0 +1,110 @@
+"use strict";
+
+//Zum zeichen
+class Renderer{
+    constructor(element){
+        this.element = element;
+        this.setup();
+    }
+
+    setup(){
+        //Ball hinzufügen
+        let box = document.createElement("div");
+
+        //Position
+        box.style.position ="absolute";
+        box.style.top="20px";
+        box.style.left="20px";
+
+        //Inhalt
+        box.style.backgroundColor ="red";
+        box.style.width = "20px";
+        box.style.height ="20px";
+
+        this.element.appendChild(box);
+        this.box = box;
+    }
+
+    //Bewegung
+    render(position){
+        this.box.style.top = position + "px";
+    }
+}
+//Bewegungs Logik
+class Box {
+    constructor(){
+        this.position = 0;
+        this.speed = 0;
+
+        /**
+         * Negativer Speed: Nach oben
+         * Positiver Spedd: Nach unten
+         */
+    }
+    //Bewegungs Logik nach unten
+    runLoop(){
+        this.speed++;
+        this.position = this.position + this.speed;
+    }
+    //Logik für nach oben
+    moveUp(){
+        this.speed = -20;
+    }
+}
+
+class Game {
+    constructor(element) {
+        
+        this.renderer = new Renderer(element);
+        this.box = new Box();
+        
+        
+        this.element = element;
+        //Läuft das Spiel
+        this.isRunning = true;
+        this.setup();
+    }
+
+    //Klick auf Box
+    setup(){
+        this.element.addEventListener("click", () =>{
+            this.box.moveUp();
+        }, false);
+    }
+
+    start(){ 
+        //Highscore
+        let counter = 0;
+
+        let timer = setInterval(() =>{
+            counter++;
+            this.box.runLoop();
+            //Obere Rand
+            if(this.box.position < 0){
+                this.isRunning = false;
+                //Intervall abbrechen
+                clearInterval(timer);
+                alert("Oberer Rand erreicht: Gameover, " + counter + " Punkte!");
+                this.box.position = this.box.position + 20;
+                
+            }
+            //Unterre Rand
+            if(this.box.position + 20 > this.element.clientHeight){
+                this.isRunning = false;
+                //Intervall abbrechen
+                clearInterval(timer);
+                alert("Unterer Rand erreicht: Gameover, " + counter + " Punkte!");
+                this.box.position = this.box.position - 20;
+                
+            }
+        this.renderer.render(this.box.position);
+        }, 40);
+        console.log(timer);
+    }
+
+}
+//Übergabe des Spielfelds
+let game = new Game(document.getElementById("game"));
+game.start();
+
+ 
--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,11 @@
 "use strict";
 
+let img = document.getElementById("popup_img-1");
+let img2 = document.getElementById("popup_img-2");
+let title = document.getElementById("popup_name");
+let text = document.getElementById("popup_description")
+let content = document.getElementById("popup__content-div");
+
 class Popup{
 
     constructor(id) {
@@ -8,41 +14,69 @@ class Popup{
         this.data = [
             {img: "img/Game-1.png", name: "click game", description: "A small JavaScript game."},
             {img: "img/Game-2.png", name: "Dino Jump", description: "A small JavaScript game where a dinosaur has to jump over cactuses."},
-            {img: "img/HP.jpg", name: "Portfolio", description: "A first portfolio page for me."}
+            {img: "img/HP.jpg", name: "Portfolio", description: "A first portfolio page for me."},
+            {img: "img/FF-Michelhausen.jpg", name: "FF-Michelhausen", description: "Modern website for the Michelhausen fire brigade. This project was implemented with Sass, JavaScript, PHP and Wordpress."},
+            {img: "img/heartbeat.jpg", name: "HeartBeatClubbing", description: "A website for the purchase of defibrillators for the Tulln district."}
         ]
         
     }
 
     start(){
         console.log("start " + this.theId)
-        let img = document.getElementById("popup_img-1");
-        let img2 = document.getElementById("popup_img-2");
-        let name = document.getElementById("popup_name");
-        let text = document.getElementById("popup_description")
 
         if(this.theId === "1"){
+            document.getElementById("popup2").id ="popup";
             img.src = this.data[0].img;
             img2.src = this.data[0].img;
-            name.innerHTML = this.data[0].name;
+            title.innerHTML = this.data[0].name;
             text.innerHTML = this.data[0].description; 
+            
+            content.id = "game";
+            content.innerHTML ="";
+            let game_script = document.createElement("script");
+            game_script.setAttribute('src', './js/clickgame.js');
+            content.appendChild(game_script);
+    
         }
         else if(this.theId === "2"){
+            document.getElementById("popup1").id ="popup";
             img.src = this.data[1].img;
             img2.src = this.data[1].img;
-            name.innerHTML = this.data[1].name;
+            title.innerHTML = this.data[1].name;
             text.innerHTML = this.data[1].description; 
+            
         }
         else if(this.theId === "3"){
+            document.getElementById("popup1").id ="popup";
             img.src = this.data[2].img;
             img2.src = this.data[2].img;
-            name.innerHTML = this.data[2].name;
+            title.innerHTML = this.data[2].name;
             text.innerHTML = this.data[2].description; 
+            
+        }
+        else if(this.theId === "4"){
+            document.getElementById("popup1").id ="popup";
+            img.src = this.data[3].img;
+            img2.src = this.data[3].img;
+            title.innerHTML = this.data[3].name;
+            text.innerHTML = this.data[3].description; 
+            
+        }
+        else if(this.theId === "5"){
+            document.getElementById("popup1").id ="popup";
+            img.src = this.data[4].img;
+            img2.src = this.data[4].img;
+            title.innerHTML = this.data[4].name;
+            text.innerHTML = this.data[4].description; 
+            
         }
         else {
+            document.getElementById("popup1").id ="popup";
             img.src = this.data[0].img;
             img2.src = this.data[0].img;
-            name.innerHTML = this.data[0].name;
+            title.innerHTML = this.data[0].name;
             text.innerHTML = this.data[0].description; 
+            
         }
 
     }
@@ -55,7 +89,26 @@ document.querySelectorAll(".project__btn").forEach(item =>{
     item.addEventListener("click", event => {
         let popup = new Popup(item.id);
         popup.start();
+
     })
 });
+
+document.getElementById("popup__close").addEventListener("click", event => {
+    document.getElementById("popup").id = "popup1";
+});
+
+document.getElementById("popup__close2").addEventListener("click", event => {
+    document.getElementById("popup").id = "popup2";
+    content.id = "popup__content-div";
+    content.removeChild(content.firstChild);
+    location.reload();
+    
+});
+
+    
+
+
+
+
 
 
